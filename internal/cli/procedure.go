203c811eb383efa1e9dfb8c3a4fd72b438ebf0aa
Joe Purdy <joe@purdy.dev>
2019-12-25 15:22:13 -0800

    allow for custom data in procedures

--- internal/cli/procedure.go
+++ internal/cli/procedure.go
@@ -1,46 +1,90 @@
 package cli
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"text/template"
 
+	"github.com/pkg/errors"
 	"github.com/strongdm/comply/internal/config"
 	"github.com/strongdm/comply/internal/model"
 	"github.com/urfave/cli"
+	"gopkg.in/yaml.v2"
 )
 
 var procedureCommand = cli.Command{
 	Name:      "procedure",
 	ShortName: "proc",
 	Usage:     "create ticket by procedure ID",
-	ArgsUsage: "procedureID",
+	ArgsUsage: "procedureID data.yaml",
 	Action:    procedureAction,
 	Before:    beforeAll(projectMustExist, ticketingMustBeConfigured),
 }
 
 func procedureAction(c *cli.Context) error {
+	var (
+		dataBytes []byte
+		err       error
+	)
+
+	switch len(c.Args()) {
+	case 0:
+		return cli.NewExitError("provide a procedure ID", 1)
+	case 1:
+	case 2:
+		dataBytes, err = ioutil.ReadFile(c.Args().Get(1))
+		if err != nil {
+			return cli.NewExitError(err.Error(), 1)
+		}
+	default:
+		return cli.NewExitError("too many args provided", 1)
+	}
+
+	procedureID := c.Args().First()
+
+	if err := renderProcedure(procedureID, dataBytes); err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	return nil
+}
+
+func renderProcedure(id string, dataBytes []byte) error {
 	procedures, err := model.ReadProcedures()
 	if err != nil {
 		return err
 	}
 
-	if c.NArg() != 1 {
-		return cli.NewExitError("provide a procedure ID", 1)
+	// unmarshal data
+	data := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(dataBytes, &data)
+	if err != nil {
+		return err
 	}
-
-	procedureID := c.Args().First()
 
 	ts, err := config.Config().TicketSystem()
 	if err != nil {
-		return cli.NewExitError("error in ticket system configuration", 1)
+		return errors.New("error in ticket system configuration")
 	}
 
 	tp := model.GetPlugin(model.TicketSystem(ts))
 
 	for _, procedure := range procedures {
-		if procedure.ID == procedureID {
+		if procedure.ID == id {
+			// render body from data
+			var w bytes.Buffer
+			bodyTemplate, err := template.New("body").Parse(procedure.Body)
+			if err != nil {
+				w.WriteString(fmt.Sprintf("# Error processing template:\n\n%s\n", err.Error()))
+			} else {
+				bodyTemplate.Execute(&w, data)
+			}
+			renderedBody := w.String()
+
 			err = tp.Create(&model.Ticket{
 				Name: procedure.Name,
-				Body: fmt.Sprintf("%s\n\n\n---\nProcedure-ID: %s", procedure.Body, procedure.ID),
+				Body: fmt.Sprintf("%s\n\n\n---\nProcedure-ID: %s", renderedBody, procedure.ID),
 			}, []string{"comply", "comply-procedure"})
 			if err != nil {
 				return err
@@ -49,5 +93,5 @@ func procedureAction(c *cli.Context) error {
 		}
 	}
 
-	return cli.NewExitError(fmt.Sprintf("unknown procedure ID: %s", procedureID), 1)
+	return errors.New(fmt.Sprintf("unknown procedure ID: %s", id))
 }
